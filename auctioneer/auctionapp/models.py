53d22c0e54da70867b4f0febf430bf67ec7fe993
Pallav Mahamana <spsec.pallav@gmail.com>
2018-07-05 22:20:44 +0530

    end auction datetime in models.py is one day after current time

--- auctioneer/auctionapp/models.py
+++ auctioneer/auctionapp/models.py
@@ -2,17 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Create your models here.
 
 class AuctionItem(models.Model):
+	def get_end_auc_datetime():
+		return datetime.now() + timedelta(days=1)
 	title = models.CharField(max_length=50)
 	desc = models.TextField()
 	created_on = models.DateTimeField('Created on', auto_now_add=True)
 	start_auc_datetime = models.DateTimeField(default=datetime.now)
-	end_auc_datetime = models.DateTimeField()
-
+	end_auc_datetime = models.DateTimeField(default=get_end_auc_datetime)
 
 	def __str__(self):
 		return self.title
